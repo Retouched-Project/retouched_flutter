6eb186821996a7fe617cfdde654b99afb2b6c647
Foica David <superh552@gmail.com>
2026-05-16 02:10:30 +0300

    release 1.0.3

--- lib/main_games_page.dart
+++ lib/main_games_page.dart
@@ -578,7 +578,7 @@ class _GamesPageState extends State<GamesPage>
             ),
             const SizedBox(height: 5),
             const Text(
-              'Version 1.0.2',
+              'Version 1.0.3',
               style: TextStyle(color: Color(0xFFAAAAAA), fontSize: 16),
             ),
             const SizedBox(height: 40),
